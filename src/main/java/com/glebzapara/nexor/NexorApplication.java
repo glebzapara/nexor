7337265b0697c9e482eadebb662b500f8b3540ae
Gleb Zapara <gleb.zapara2008@gmail.com>
2026-04-17 02:26:48 +0300

    first commit

---
+++ src/main/java/com/glebzapara/nexor/NexorApplication.java
@@ -0,0 +1,13 @@
+package com.glebzapara.nexor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/glebzapara/nexor/NexorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glebzapara.nexor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
